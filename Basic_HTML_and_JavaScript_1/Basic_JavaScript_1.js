043e8890b52d836ad0b5374a4485667e496f464b
dhavap <50911475+dhavap@users.noreply.github.com>
2020-12-08 19:57:28 -0800

    Delete Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-alert("Hello! I am running solo!");
-
-function My_First_Function() {
-    var String = "Kiss me, I'm Irish!";
-    var result = String.fontcolor("green");
-    document.getElementById("Irish").innerHTML = result;
-}
